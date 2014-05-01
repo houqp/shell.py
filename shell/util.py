34a2fac19b9711ce341fec31c1e09370bfb34bec
Qingping Hou <dave2008713@gmail.com>
2014-05-01 18:17:11 -0400

    util: convert string to bytes before writing to file

--- shell/util.py
+++ shell/util.py
@@ -3,9 +3,11 @@
 
 import tempfile
 
+
 def str_to_pipe(s):
     input_pipe = tempfile.SpooledTemporaryFile()
+    if isinstance(s, str):
+        s = s.encode('utf-8')
     input_pipe.write(s)
     input_pipe.seek(0)
     return input_pipe
-
