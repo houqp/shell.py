e1561e8f20f243895d417d148460a8ec9c19e1ef
Qingping Hou <dave2008713@gmail.com>
2014-05-01 19:19:08 -0400

    make tests compatible with py3

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+from __future__ import unicode_literals
 import os
 import unittest
-
-import shell
 import subprocess
+import shell
 
 
 class TestShellStyle(unittest.TestCase):
@@ -23,16 +23,24 @@ class TestShellStyle(unittest.TestCase):
 
     def test_single_run_stdout(self):
         re = shell.p('echo hello shell.py').stdout()
-        self.assertEqual(re, 'hello shell.py\n')
-        re = shell.ex('echo 你好 shell.py').stdout()
-        self.assertEqual(re, '你好 shell.py\n')
+        self.assertEqual(re, b'hello shell.py\n')
+
+        cmd_str = 'echo 你好 shell.py'
+        try:
+            unicode
+        except NameError:
+            pass
+        else:
+            cmd_str = cmd_str.encode('utf-8')
+        re = shell.ex(cmd_str).stdout()
+        self.assertEqual(re, '你好 shell.py\n'.encode('utf-8'))
 
     def test_multiple_ex(self):
         re = shell.ex_all([
             'echo hello',
             'echo world'])
-        self.assertEqual(re[0].stdout(), 'hello\n')
-        self.assertEqual(re[1].stdout(), 'world\n')
+        self.assertEqual(re[0].stdout(), b'hello\n')
+        self.assertEqual(re[1].stdout(), b'world\n')
 
     def test_single_run_stderr(self):
         bad_path = 'wtf#noneexist#dir#yay'
@@ -44,20 +52,20 @@ class TestShellStyle(unittest.TestCase):
 
     def test_simple_pipe(self):
         pipeline = shell.p('echo yes no').p("awk '{print $1}'")
-        self.assertEqual(pipeline.stdout(), 'yes\n')
+        self.assertEqual(pipeline.stdout(), b'yes\n')
         self.assertEqual(pipeline.re(), 0)
 
     def test_simple_pipe_run(self):
         pipeline = shell.p('echo yes no').p("awk '{print $1}'")
         pipeline.run()
-        self.assertEqual(pipeline.stdout(), 'yes\n')
+        self.assertEqual(pipeline.stdout(), b'yes\n')
         self.assertEqual(pipeline.re(), 0)
 
     def test_instream(self):
         in_str = open(self.ifconfig_out_path).read()
         self.assertEqual(
             shell.instream(in_str).p('grep eth0').stdout(),
-            'eth0      Link encap:Ethernet  HWaddr 00:30:ac:c9:2e:f4\n')
+            b'eth0      Link encap:Ethernet  HWaddr 00:30:ac:c9:2e:f4\n')
 
     def test_chain_of_pipe(self):
         pipeline = shell.p('cat {0}'.format(self.ifconfig_out_path))\
@@ -65,7 +73,7 @@ class TestShellStyle(unittest.TestCase):
                         .p("grep inet")\
                         .p("awk '{print $2}'")\
                         .p("cut -d: -f 2")
-        self.assertEqual(pipeline.stdout(), '192.168.116.101\n')
+        self.assertEqual(pipeline.stdout(), b'192.168.116.101\n')
         self.assertEqual(pipeline.re(), 0)
         pipeline = (shell
                     .p('cat {0}'.format(self.ifconfig_out_path))
@@ -73,7 +81,7 @@ class TestShellStyle(unittest.TestCase):
                     .p("grep inet")
                     .p("awk '{print $2}'")
                     .p("cut -d: -f 2"))
-        self.assertEqual(pipeline.stdout(), '127.0.0.1\n')
+        self.assertEqual(pipeline.stdout(), b'127.0.0.1\n')
         self.assertEqual(pipeline.re(), 0)
 
     def test_chain_of_pipe_using_operator_overloading(self):
@@ -82,7 +90,7 @@ class TestShellStyle(unittest.TestCase):
                     | shell.RunCmd("grep inet")
                     | shell.RunCmd("awk '{print $2}'")
                     | shell.RunCmd("cut -d: -f 2"))
-        self.assertEqual(pipeline.stdout(), '192.168.116.101\n')
+        self.assertEqual(pipeline.stdout(), b'192.168.116.101\n')
         self.assertEqual(pipeline.re(), 0)
 
         pipeline = (shell.RunCmd('cat {0}'.format(self.ifconfig_out_path)) |
@@ -90,7 +98,7 @@ class TestShellStyle(unittest.TestCase):
                                  "grep inet" |
                                  "awk '{print $2}'" |
                                  "cut -d: -f 2")
-        self.assertEqual(pipeline.stdout(), '127.0.0.1\n')
+        self.assertEqual(pipeline.stdout(), b'127.0.0.1\n')
         self.assertEqual(pipeline.re(), 0)
 
     def test_pipe_with_cmd_list(self):
@@ -99,24 +107,25 @@ class TestShellStyle(unittest.TestCase):
                                    'grep inet',
                                    'awk "{print $2}"',
                                    'cut -d: -f 2'])
-        self.assertEqual(pipeline.stdout(), '192.168.116.101\n')
+        self.assertEqual(pipeline.stdout(), b'192.168.116.101\n')
         self.assertEqual(pipeline.re(), 0)
         pipeline = shell.p(['cat {0}'.format(self.ifconfig_out_path),
                             'grep -A 1 eth0',
                             'grep inet',
                             'awk "{print $2}"',
                             'cut -d: -f 2'])
-        self.assertEqual(pipeline.stdout(), '192.168.116.101\n')
+        self.assertEqual(pipeline.stdout(), b'192.168.116.101\n')
         self.assertEqual(pipeline.re(), 0)
 
     def test_ex_pipe(self):
         out = shell.ex('echo -e "123\n456\n789"').p('grep 4').stdout()
-        self.assertEqual(out, '456\n')
+        self.assertEqual(out, b'456\n')
 
     def test_expand_tilde(self):
         task = shell.ex('echo ~')
         self.assertEqual(task.re(), 0)
-        self.assertEqual(task.stdout(), os.path.expanduser('~')+'\n')
+        self.assertEqual(task.stdout(),
+                         (os.path.expanduser('~')+'\n').encode('utf-8'))
 
 
 if __name__ == "__main__":
