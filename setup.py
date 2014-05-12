391debf497cc466a319532bf73aad1f38c1e6ae9
Qingping Hou <dave2008713@gmail.com>
2014-05-11 23:26:23 -0400

    remove py2.6 from test matrix

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
